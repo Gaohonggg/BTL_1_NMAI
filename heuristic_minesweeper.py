3593ce7c97afe20caa90e761908233d644862487
TuNguyen2212 <hoangtu22122004@gmail.com>
2025-02-19 13:45:37 +0700

    add comments for code explaining

--- heuristic_minesweeper.py
+++ heuristic_minesweeper.py
@@ -13,28 +13,30 @@ get_mines_randomly_2d = np.unravel_index(get_mines_randomly, grid.shape)
 grid[get_mines_randomly_2d] = -1
 
 ## Random start point
-x ,y = None, None
+x_start ,y_start = None, None
 while True:
-    x, y = np.random.randint(0, x_axis), np.random.randint(0, y_axis)
-    if grid[x, y] == 9:
-        grid[x, y] = 0
+    x_start, _start = np.random.randint(0, x_axis), np.random.randint(0, y_axis)
+    if grid[x_start, y_start] == 9:
+        grid[x_start, y_start] = 0
         break
 
 ## untouch_lst : list of unchosen squares
 get_mines_randomly_2d = np.transpose(get_mines_randomly_2d).tolist()
 untouch_lst = [(x,y) for x in range(x_axis) for y in range(y_axis)]
-untouch_lst.remove((x,y))
+untouch_lst.remove((x_start,y_start))
 
 
+## spread : the action occur when click a square with no mine around, all around square is auto-click
 def spread(x,y,grid,queue,around = []):
     for i,j in around:
         a,b = x+i,y+j
         if grid[a][b] == 9:
             check_around(a,b,grid,queue)
 
-
+## check_around : the action when click a square
 def check_around(x,y,grid,queue = []):
     if not grid[x][y] == -1:
+        ## When click a no-mine square, it check around and get the number of mines around
         count = 0
         check_p = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
         for i,j in check_p[:]:
@@ -46,11 +48,13 @@ def check_around(x,y,grid,queue = []):
                     check_p.remove((i,j))
             else: 
                 check_p.remove((i,j))
+        ## If count = 0 than spread
         if count == 0:
             grid[x][y] = 0
             spread(x,y,grid, queue,check_p)
             if (x,y) in untouch_lst:
                 untouch_lst.remove((x,y))
+        ## If not just reveal the count
         else:
             grid[x][y] = count
             if (x,y) in untouch_lst:
@@ -58,39 +62,13 @@ def check_around(x,y,grid,queue = []):
             queue.append((x,y,check_p))
     return queue ,grid
 
+## queue : contains a list of tuples, each tuple is a point and around points used for check
 queue=[]
-# # # # specific example
-# x_axis , y_axis = 5,5
-# grid = np.array([[9,2,9,9,9],
-#                  [9,9,2,9,2],
-#                  [9,3,3,9,9],
-#                  [1,9,2,2,2],
-#                  [9,1,1,1,9]])
-# not_9_indices = np.where(grid != 9)
-# list_number = list(zip(not_9_indices[0], not_9_indices[1]))
-# untouch_lst = [(x,y) for x in range(x_axis) for y in range(y_axis)]
-# queue=[]
-# for x,y in list_number:
-#     check_p = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
-#     for i,j in check_p[:]:
-#         a,b=x+i,y+j
-#         if a in range(0,x_axis) and b in range(0,y_axis):
-#             if grid[a,b] in range(0,9):
-#                 check_p.remove((i,j))
-#         else: check_p.remove((i,j))
-#     queue.append((x,y,check_p))
-#     untouch_lst.remove((x,y))
-# def check_around(x,y,grid,queue = []):
-#     grid[x][y]=0
-#     untouch_lst.remove((x,y))
-#     return [] , grid
-# ################################
 
-# queue = []
-
+## mine_found : list of mine found by the algorithm
 mine_found = []
 
-def shrink_p(queue):
+def shrink_p(queue): ## delete some around points in tuple that are already clicked by the algorithm
     for x , y, p in queue[:]:
         if p:
             for i,j in p[:]:
@@ -100,28 +78,29 @@ def shrink_p(queue):
         else: queue.remove((x,y,p))
     return queue
 
-queue, grid = check_around(x,y,grid,[])
+## Actions after first random click at random start point by the algorithm
+queue, grid = check_around(x_start,y_start,grid,[])
 queue =  shrink_p(queue)
-steps = [((x,y),grid)]
-steps_to_win = []
+steps = [((x_start,y_start),grid)] ## steps: list of tuples, each tuple is a step move and state after that move
+steps_to_win = [] ## wining step move list of algorithm
 
 # Heuristics
-# check around if number == the left square aka that is the mine, and if the unknow square = mine found then press the left square
 def check_mine_base_on_square_left(queue, grid, mine_found):
     k = True
     while k:
         k= False
         for x,y ,p in queue[:]:
-            mine = 0
-            not_mine = 0
+            mine = 0 # number of already found mines
+            unknown = 0 # number of unknown squares
             for i,j in p[:]:
                 a,b = x+i,y+j
                 if (a,b) in mine_found:
                     mine += 1
                 elif (a,b) in untouch_lst:
-                    not_mine += 1
+                    unknown += 1
                 else: p.remove((i,j))
             if mine == grid[x,y]:
+                # if all mines are found => other squares near x,y is sure no-bomb => click all other squares
                 k= True
                 g= grid.copy()
                 for i,j in p[:]:
@@ -132,7 +111,8 @@ def check_mine_base_on_square_left(queue, grid, mine_found):
                         steps_to_win.append((a,b))
                         queue += shrink_p(new_queue)
                 queue.remove((x,y,p))
-            elif not_mine <= grid[x][y] - mine:
+            elif unknown <= grid[x][y] - mine:
+                # if unknown <= numbers of bomb left, all unknowns squares are bombs, add to mine_found
                 k = True
                 for i,j in p[:]:
                     a,b = x+i,y+j
@@ -141,17 +121,21 @@ def check_mine_base_on_square_left(queue, grid, mine_found):
                 queue.remove((x,y,p))
 
 
-
+## Run the rest algorithm
 start_time = time.time()
 check_mine_base_on_square_left(queue,grid,mine_found)
 end_time = time.time()
 
 print(mine_found)
 
+## Start unsuccessful algorithm : needs to choose random square to continue
 if not mine_found:
     print("Your first step, step on a number, must choose a random square")
 if len(mine_found) != 50:
     print("You get a case that u need to make a gacha square to keep moving")
+## End unsuccessful algorithm
+
+## Steps list output
 print("step to win: " + str(steps_to_win))
 print("Time to run: " + str(end_time-start_time))
 def navigate_list(lst):
